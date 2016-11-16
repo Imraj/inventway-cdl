d41c91d72f6edbb693c3854be1e2253a8f514a72
Mujahid Raji <imraj3@live.co.uk>
2016-11-16 01:37:57 +0100

    latest commit

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ require('./models/UContact');
 require('./models/Payment');
 
 
-//mongoose.connect("mongodb://nyc2017-cdl:Newyork190-cdl@ec2-54-183-254-91.us-west-1.compute.amazonaws.com:27017/inventwayCDL");
+mongoose.connect("mongodb://nyc2017-cdl:Newyork190-cdl@ec2-54-183-254-91.us-west-1.compute.amazonaws.com:27017/inventwayCDL");
 
 //mongoose.connect("mongodb://mhadiab:mhadiab85@mhadi85-9285.mongo.dbs.appsdeck.eu:30987/mhadi85-9285");
 //mongodb://<user>:<password>@mhadi85-9285.mongo.dbs.appsdeck.eu:30987/mhadi85-9285
